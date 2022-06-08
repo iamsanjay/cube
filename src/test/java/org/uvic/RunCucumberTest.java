42b7548f06125e65b7b1f95542fc1f709033dad3
Sanjay Dutt <sanjaydutt@uvic.ca>
2022-06-08 10:29:10 -0700

    cube project

---
+++ src/test/java/org/uvic/RunCucumberTest.java
@@ -0,0 +1,17 @@
+package org.uvic;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("org/uvic")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(key=GLUE_PROPERTY_NAME, value = "cube")
+public class RunCucumberTest {
+}
